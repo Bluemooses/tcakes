6b5b0d19b9b1c84c1f22127e78a280ce75b01127
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2020-08-17 18:27:05 -0500

    added auth

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">Hello</header>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
